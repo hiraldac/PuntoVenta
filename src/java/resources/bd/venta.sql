f5b2b534dbc84802f856e55789f411596a99163c
hiraldac <43788284+hiraldac@users.noreply.github.com>
2019-12-03 18:33:55 -0600

    Venta.sql
    Actualización del script de la base de datos

--- src/java/resources/bd/venta.sql
+++ src/java/resources/bd/venta.sql
@@ -2,20 +2,18 @@
  * Autor:  hiralda
  * Email: castroc.hiralda@gmail.com
  * Creación: 26/11/2019
- * Modificación: 26/11/2019
+ * Modificación: 3/12/2019
  */
-
 CREATE TABLE public.usuario
 (
-    idusuario bigint NOT NULL,
+    idusuario bigserial NOT NULL,
     nombre text NOT NULL,
     tipo text NOT NULL,
     contrasena text NOT NULL,
     usuario text NOT NULL,
     CONSTRAINT usuario_pkey PRIMARY KEY (idusuario),
     CONSTRAINT uq_usuario UNIQUE (usuario)
-
-)
+);
 
 CREATE TABLE public.producto
 (
@@ -26,29 +24,25 @@ CREATE TABLE public.producto
     precio numeric NOT NULL,
     existencia integer NOT NULL,
     CONSTRAINT producto_pkey PRIMARY KEY (idproducto)
-)
+);
 
 CREATE TABLE public.venta
 (
-    idventa bigint NOT NULL ,
-    hora timestamp without time zone NOT NULL,
+    idventa biginit NOT NULL ,
+    fecha timestamp without time zone NOT NULL,
     total numeric NOT NULL,
     usuarioid bigint NOT NULL,
     CONSTRAINT venta_pkey PRIMARY KEY (idventa)
-)
+);
 
 CREATE TABLE public.detalle
 (
-    ventaid bigint NOT NULL,
+    ventaid biginit NOT NULL,
     cantidad integer NOT NULL,
     productoid text NOT NULL,
-    CONSTRAINT detalle_pkey PRIMARY KEY (ventaid, cantidad),
+    CONSTRAINT detalle_pkey PRIMARY KEY (ventaid, productoid),
     CONSTRAINT fk_producto FOREIGN KEY (productoid)
-        REFERENCES public.producto (idproducto) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
+        REFERENCES public.producto (idproducto),
     CONSTRAINT fk_venta FOREIGN KEY (ventaid)
-        REFERENCES public.venta (idventa) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
+        REFERENCES public.venta (idventa) 
+);
